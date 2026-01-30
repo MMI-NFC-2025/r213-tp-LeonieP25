4e9d4c9fe1d85df9985b29243903b50a3782a410
LeonieP25 <leonie.pruniaux@edu.univ-fcomte.fr>
2026-01-30 08:58:57 +0100

    Affichage et modifications de getOffre

---
+++ src/js/backend.mjs
@@ -0,0 +1,18 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function getImageUrl(record, imageField) {  
+    return pb.files.getUrl(record, imageField);  
+}
+
+
+export async function getOffres() {
+	try {
+		return  await pb.collection('maison').getFullList({
+			sort: '-created',
+		});
+	} catch (error) {
+		console.log('Une erreur est survenue en lisant la liste des maisons', error);
+		return [];
+	}
+}
