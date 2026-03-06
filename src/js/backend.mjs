398afb57f8cf3de5a960ceb52c90c2140c14675a
LeonieP25 <leonie.pruniaux@edu.univ-fcomte.fr>
2026-03-06 08:14:19 +0100

    Ajout de favorite et favorite_filled

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -1,5 +1,4 @@
-import PocketBase from 'pocketbase';
-const pb = new PocketBase('http://127.0.0.1:8090');
+import pb from '../utils/pb';
 
 export async function getImageUrl(record, imageField) {  
     return pb.files.getURL(record, imageField);  
